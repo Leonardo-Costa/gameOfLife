3726d2ae133d358f701d392ed199997fc20e9a0f
leonardo <leonardo.costa@unifesp.br>
2023-09-24 18:40:22 -0300

    feat: add geracao de dados condicional

--- src/python/main.py
+++ src/python/main.py
@@ -1,8 +1,13 @@
+import sys
+import os
 from generate_data import generateData
 from analyze_data import analyzeData
 
 def main():
-    #generateData()
+    data_file = '../../data/csv/performance_data.csv'
+
+    if (len(sys.argv) > 1 and sys.argv[1] == 'force') or not os.path.exists(data_file):
+        generateData()
     analyzeData()
 
 if __name__ == '__main__':
